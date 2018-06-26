18b63a9645c94b6c362dbcfea4044818e05933d9
Ponker25 <williamswensen86@gmail.com>
2018-06-25 20:41:25 -0600

    it works

--- liri.js
+++ liri.js
@@ -1,53 +1,114 @@
-console.log("liri.js is loaded");
 
-var dotenv = require("dotenv").config();
-
-var apiList = require("./keys.js");
-
+require("dotenv").config();
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+var fs = require("fs");
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2]
 
+
+// Switch statement
+switch(command) {
+    case `my-tweets`:
+        runTwitter();
+    case `spotify-this-song`:
+        runSpotify(fullInput());
+        break;
+    case `movie-this`:
+        runMovie(fullInput());
+        break;
+    case `do-what-it-says`:
+        runDoWhatItSays();
+        break;
+    default:
+        console.log("Liri didnt understand, try using one of the folloing commands")
+        console.log("------------------------------------")
+        console.log("my-tweets");
+        console.log("spotify-this-song `Your song title`");
+        console.log("movie-this `Your Movie Title`")
+        console.log(`do-what-it-says`);
+}
+
+function fullInput() {
+    var userInput = process.argv[3];
+    for (var i = 4; i < process.argv.length; i++) {
+        userInput = userInput + " " + process.argv[i];
+    }
+    return userInput;
+}
 // TWITTER
 
-var Twitter = require('twitter');
-// my tweets (will show the last 20 tweets) command = node liri.js my-tweets
-  var params = {screen_name: 'nodejs'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-    }
-  });
+function runTwitter() {
+    var params = { screen_name: 'sltrib' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function (element) {
+                console.log("\n=================================\n" + element.created_at);
+                console.log(element.text);
+            });
+        }
+    });
+}
 
-// var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-
-console.log("this is a test");
-console.log(Twitter)
 
 // SPOTIFY
 
-// var Spotify = require('node-spotify-api');
-//  spotify this song = (Show Artists, The Song Name, A preview link of the song from spotify, the Album, IF NO SONG DEFAULT TO "The Sign" by Ace of Base.)
+function runSpotify(songTitle) {
 
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//      console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
-
+    //check for user input
+    if(songTitle === null || typeof songTitle === 'undefined' || songTitle === undefined ){
+        songTitle = "The+Sign";
+    }    
+    
+    spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+    if (err) {
+        console.log('Error occurred: ' + err);
+    } 
+    for (i = 0; i < data.tracks.items.length; i++) {
+        // console.log(data.tracks.items[1])
+        console.log("-----------------------------");
+        console.log("Song-Title:" + songTitle);
+        console.log("Artist Name: " + data.tracks.items[i].album.artists[0].name);
+        console.log("Link to Spotify: " + data.tracks.items[i].external_urls.spotify);
+        console.log("Album Title: "+ data.tracks.items[i].album.name)
+        console.log("----------------------------");
+        }
+    });
+}
 
 // OMD MOVIE SEARCH
+function runMovie(movieTitle) {
+
+    // Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("----------------------------------------")
+    console.log("Movie Title: " + JSON.parse(body).Title)
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("Movie Rating: " + JSON.parse(body).Rated);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value )
+    console.log("Country Where Movie Was Produced: " + JSON.parse(body).Country)
+    console.log("Language Produced: " + JSON.parse(body).Language)
+    console.log("Movie Plot: " + JSON.parse(body).Plot)
+    console.log("List of Actors: " + JSON.parse(body).Actors)
+    console.log("------------------------------------------")
+
+  }
+});
+}
 // movie-this (command = node liri.js <movie name here>)
 
 // var request = require("request");
@@ -73,6 +134,22 @@ console.log(Twitter)
 // });
 
 // DO WHAT IT SAYS COMMAND 
+function runDoWhatItSays() {
+    console.log("doing it");
+
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error) {
+            return console.log(error);
+        }
+ 
+        var randomChoice = data.split(",");
+        console.log(randomChoice);
+        if(randomChoice[0] === "spotify-this-song") {
+            runSpotify(randomChoice[1]);
+        }
+        
+    })
+}
 // do-what-it-says = (command = node liri do-what-it-says)
  
 // should utilize the random.txt folder to spotify the song "I want it that way"
@@ -83,4 +160,3 @@ console.log(Twitter)
 // BONUS: Log the data to the terminal bash window, output the data to the file log.txt.
 // * Make sure you append each command you run to the `log.txt` file. 
 
-// * Do not overwrite your file each time you run a command.
